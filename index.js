8d1492674b9207fd23bfc782ccfb8f267aed856c
Daniel Zhang <dzhangwh@gmail.com>
2022-01-11 17:57:19 -0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+    let free = 'This one is on me!'
+    return free
+  }
+  else if (someNumber >= 2000 && someNumber < 2500) {
+    let extraCharge = 'I will gladly take your thirty bucks.'
+    return extraCharge
+  }
+  else if (someNumber >= 2500) {
+    let tooFar = 'No can do.'
+    return tooFar
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  const checkCity = someCity == 'NYC' ? "Ok, sounds good." : "No go.";
+  return checkCity
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+     let tip;
+
+    switch(someTip) {
+
+      case 'generous':
+        tip = "Thank you so much."
+        break;
+
+      case 'not as generous':
+        tip = 'Thank you.'
+        break;
+      
+      default:
+        tip = 'Bye.'
+        break;
+    }
+  return tip
 }
